65e91395fc2d9be71b98cc9d548cb5f065d7084f
Don Lee <git@mrdonlee.addy.io>
2023-12-23 12:38:22 +0000

    update: adds merge sort and binary search files

---
+++ UEL/CN5005 - Data Structures and Algorithms/Week 04/MergeSort.java
@@ -0,0 +1,16 @@
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] arr = { 12, 11, 13, 5, 6, 7 };
+
+
+    }
+
+    public static void sort(int[] array) {
+        int size = array.length;
+        int mid;
+
+        if (size > 1) {
+            mid = array / 2;
+        }
+    }
+}
