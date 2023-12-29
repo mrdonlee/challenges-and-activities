59e734a007698d41c41840613859add6e954b29c
Don Lee <git@mrdonlee.addy.io>
2023-12-29 11:15:44 +0000

    update: adds user edit menu

--- UEL/CN5008 - System Administration/BYOD/script.ps1
+++ UEL/CN5008 - System Administration/BYOD/script.ps1
@@ -16,3 +16,44 @@ function Display-Menu {
     Write-Host ""
     $option = Read-Host "Please select an option" # Gets input from the user and store in option variable.
 }
+
+# The function used to display edit options for a specified user.
+function DisplayEditMenu {
+    Clear-Host
+
+    Write-Host "========================================================"
+    Write-Host "|                CHANGE USER DETAILS                   |"
+    Write-Host "========================================================"
+    Write-Host ""
+    Write-Host "Please select an option for modification:"
+    Write-Host ""
+    Write-Host "[1] Change full name:"
+    Write-Host "[2] Change password:"
+    Write-Host "[0] Exit"
+    Write-Host ""
+    $mod_option = Read-Host "Please select an option"
+
+    while ($true) {
+        switch ($mod_option) {
+            0 {
+                Write-Host "Exiting..."
+                break
+            }
+            1 {
+                $full_name = Read-Host "Enter the new full name"
+                net user $username /fullname:"$full_name"
+                Write-Host "Full name changed to $full_name"
+                break
+            }
+            2 {
+                Write-Host "Please type in your new password"
+                $username | Set-LocalUser -Password (Read-Host -AsSecureString)
+                break
+            }
+            default {
+                Write-Host "Invalid input..."
+                $mod_option = Read-Host "Enter 1 or 2 for option, or 0 to quit"
+            }
+        }
+    }
+}
