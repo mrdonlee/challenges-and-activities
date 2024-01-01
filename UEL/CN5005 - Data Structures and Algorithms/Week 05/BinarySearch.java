6ba32b08983d15673424ef61086df9ad421d493a
Don Lee <git@mrdonlee.addy.io>
2024-01-01 16:02:30 +0000

    solution: solves week 5 - question 2

--- UEL/CN5005 - Data Structures and Algorithms/Week 05/BinarySearch.java
+++ UEL/CN5005 - Data Structures and Algorithms/Week 05/BinarySearch.java
@@ -2,15 +2,18 @@ public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {3, 4, 5, 6, 7, 8, 9};
 
-        search(8, arr);
+        search(9, arr, 0, arr.length);
     }
 
-    static int search(int n, int[] array) {
-        int mid = array.length / 2;
+    static void search(int n, int[] array, int start, int end) {
+        int mid = (end + start) / 2;
 
-        if (array[mid] == n) {
-            return mid;
+        if (array[mid] == n && mid != 0) {
+            System.out.println(mid);
+        } else if (array[mid] > n){
+            search(n, array, start, mid);
+        } else {
+            search(n, array, mid, end);
         }
-        return n;
     }
 }
